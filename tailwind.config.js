99e8d5068bf1bebd5824817f417d34097e82f49f
shemajolivetgislain <shemajolivet@gmail.com>
2023-07-26 07:42:32 +0200

    tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,25 @@ module.exports = {
       colors: {
         primaryColor: "#DD5312",
         backgroundColor: "#FFFFFF",
-        secondarybackgroundColor:"#FFF5F0",
         textColor: "#000000",
-        primaryTextColor: "",
+        secondTextColor: "#6F8290",
         danger: "#AB2424",
         correctGreen: "#24AB62",
+      },
+      keyframes: {
+        wave: {
+          "0%": { transform: "rotate(0.0deg)" },
+          "10%": { transform: "rotate(14deg)" },
+          "20%": { transform: "rotate(-8deg)" },
+          "30%": { transform: "rotate(14deg)" },
+          "40%": { transform: "rotate(-4deg)" },
+          "50%": { transform: "rotate(10.0deg)" },
+          "60%": { transform: "rotate(0.0deg)" },
+          "100%": { transform: "rotate(0.0deg)" },
+        },
+      },
+      animation: {
+        "waving-hand": "wave 2s linear infinite",
       },
     },
   },
