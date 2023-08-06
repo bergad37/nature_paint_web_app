4d053b79ff67a131fde8c26373c855f45d0f96b7
shemajolivetgislain <shemajolivet@gmail.com>
2023-08-06 09:48:00 +0200

    constant configuration

---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+export const links = [
+    {
+        title:"Home",
+        path:"/"
+    },
+    {
+        title:"About",
+        path:"/#about"
+    },
+    {
+        title:"Products",
+        path:"/#products"
+    },
+    {
+        title:"Projects",
+        path:"/#projects"
+    },
+    {
+        title:"Contact",
+        path:"/#contact"
+    }
+]
